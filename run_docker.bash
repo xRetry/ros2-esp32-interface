974b3876d943f45ce0c3d4c605bd7afd6959daf4
xRetry <marcoschwarz.at@gmail.com>
2023-03-31 13:25:29 +0200

    Change IDF version to 4.4

--- run_docker.bash
+++ run_docker.bash
@@ -1,1 +1,1 @@
-docker run -it -v .:/ws -v /home/marco/coding/ros2-esp32-interfaces:/opt/esp/idf/components/micro_ros_espidf_component/extra_packages/ros2-esp32-interfaces --net=host --privileged --name mros-idf-c mros-idf-c
+docker run -it -v .:/ws -v /home/marco/coding/ros2-esp32-interfaces:/opt/esp/idf/components/micro_ros_espidf_component/extra_packages/ros2-esp32-interfaces --net=host --privileged --name idf-mros idf-mros
