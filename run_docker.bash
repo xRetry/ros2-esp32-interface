453e1c96a9cae94dc5a5057e4eb1dc4b1f8c8080
xRetry <marcoschwarz.at@gmail.com>
2023-06-08 20:26:28 +0200

    Change docker dev env

--- run_docker.bash
+++ run_docker.bash
@@ -1,1 +1,1 @@
-docker run -it -v .:/ws -v /home/marco/coding/ros2-esp32-interfaces:/opt/esp/idf/components/micro_ros_espidf_component/extra_packages/ros2-esp32-interfaces --net=host --privileged --name idf-mros idf-mros
+docker run -it -v .:/ws -v /home/marco/coding/ros2-esp32-interfaces:/opt/esp/idf/components/micro_ros_espidf_component/extra_packages/ros2-esp32-interfaces --net=host --privileged --name idf idf
