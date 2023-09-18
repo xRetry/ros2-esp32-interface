63497e1942adbee33a50b69ceaba3bcd85853f43
xRetry <marcoschwarz.at@gmail.com>
2023-09-18 10:51:41 +0200

    Update dev-env

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -it -v .:/ws -v /home/marco/coding/ros2-esp32-messages:/opt/esp/idf/components/micro_ros_espidf_component/extra_packages/ros2-esp32-messages --net=host --privileged --name idf idf
+docker run -it -v ${PWD}:/ws -v /home/marco/coding/ros2-esp32-messages:/opt/esp/idf/components/micro_ros_espidf_component/extra_packages/ros2-esp32-messages --net=host --privileged --name idf idf
--- run_docker_build.sh
+++ run_docker_build.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v .:/ws -v /dev:/dev --net=host --privileged idf-build /bin/bash -c "idf.py menuconfig build flash"
+docker run -it --rm -v ${PWD}:/ws -v /dev:/dev --net=host --privileged idf-build /bin/bash -c "idf.py menuconfig build flash"
